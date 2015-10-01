843517076314d77077c7f1a8167874964ccc191d
josh <joshua@marinacci.org>
2015-09-30 18:39:51 -0700

    more work in mouse/keyboard input

--- demos/circle.js
+++ demos/circle.js
@@ -8,18 +8,24 @@ amino.start(function(core, stage) {
     stage.setRoot(root);
     var rect = new amino.Rect().w(200).h(250).x(0).y(0).fill("#0000ff");
     root.add(rect);
+    rect.acceptsMouseEvents = true;
+    rect.acceptsKeyboardEvents = true;
 
     var circle = new amino.Circle().radius(50)
         .fill('#ffcccc').filled(true)
         .x(100).y(100);
     //root.x(100);
 
+    circle.acceptsMouseEvents = true;
     core.on('click',circle,function() {
         console.log("clicked on the circle");
     });
-    core.on("keypress",null,function() {
-        console.log("key was pressed");
+    core.on("keypress",rect,function(e) {
+        console.log("key was pressed", e.keycode, e.printable, e.char);
     });
 
-    root.add(circle);
+    core.on('click',rect,function() {
+        console.log("clicked on the rect");
+    });
+    //root.add(circle);
 });
