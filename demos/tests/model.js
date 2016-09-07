708b120f9b49ac7a9e9b34aca4af5fd4866b67aa
Christoph Bratschi <bratschi@appamics.com>
2016-09-07 12:39:38 +0200

    moon & earth models

--- demos/tests/model.js
+++ demos/tests/model.js
@@ -29,7 +29,8 @@ gfx.start(function (err) {
     //showCubeLighted(model1, false); //color
 
     //showTriangleTexture(model1);
-    showCubeLighted(model1, true); //use texture
+    //showCubeLighted(model1, true); //use texture
+    showSphereLighted(model1);
 
     root.add(model1);
 
@@ -404,4 +405,80 @@ function showCubeLighted(model, useTexture) {
     //model.ry.anim().from(0).to(360).dur(50000).timeFunc('linear').loop(-1).start();
 }
 
-//TODO texture
+/**
+ * Sphere with lighting and texture (moon, earth).
+ */
+function showSphereLighted(model) {
+    //code from http://learningwebgl.com/cookbook/index.php/How_to_draw_a_sphere & http://learningwebgl.com/blog/?p=1253
+    const latitudeBands = 30;
+    const longitudeBands = 30;
+    const radius = 100;
+
+    var vertexPositionData = [];
+    var normalData = [];
+    var textureCoordData = [];
+
+    for (let latNumber = 0; latNumber <= latitudeBands; latNumber++) {
+        const theta = latNumber * Math.PI / latitudeBands;
+        const sinTheta = Math.sin(theta);
+        const cosTheta = Math.cos(theta);
+
+        for (var longNumber = 0; longNumber <= longitudeBands; longNumber++) {
+            var phi = longNumber * 2 * Math.PI / longitudeBands;
+            var sinPhi = Math.sin(phi);
+            var cosPhi = Math.cos(phi);
+            var x = cosPhi * sinTheta;
+            var y = cosTheta;
+            var z = sinPhi * sinTheta;
+            var u = 1 - (longNumber / longitudeBands);
+            var v = latNumber / latitudeBands;
+
+            normalData.push(x);
+            normalData.push(y);
+            normalData.push(z);
+
+            textureCoordData.push(u);
+            textureCoordData.push(v);
+
+            vertexPositionData.push(radius * x);
+            vertexPositionData.push(radius * y);
+            vertexPositionData.push(radius * z);
+        }
+    }
+
+    var indexData = [];
+
+    for (var latNumber = 0; latNumber < latitudeBands; latNumber++) {
+        for (var longNumber = 0; longNumber < longitudeBands; longNumber++) {
+            var first = (latNumber * (longitudeBands + 1)) + longNumber;
+            var second = first + longitudeBands + 1;
+
+            indexData.push(first);
+            indexData.push(second);
+            indexData.push(first + 1);
+
+            indexData.push(second);
+            indexData.push(second + 1);
+            indexData.push(first + 1);
+        }
+    }
+
+    //setup model
+    model.vertices(vertexPositionData);
+    model.indices(indexData);
+    model.normals(normalData);
+    model.uvs(textureCoordData);
+
+    const w = 2 * radius;
+    const h = w;
+
+    model.x(w).y(h);
+
+    //texture (http://planetpixelemporium.com/earth.html)
+    model.src(path.join(__dirname, 'sphere/moonmap1k.jpg'));
+    //model.src(path.join(__dirname, 'sphere/earthmap1k.jpg'));
+
+    //animate
+    model.rx.anim().from(0).to(360).dur(5000).loop(-1).start();
+    model.ry.anim().from(0).to(360).dur(5000).loop(-1).start();
+}
