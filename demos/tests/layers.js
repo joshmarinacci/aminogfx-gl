a6bd630f4a1629c1e528ec52dea9e5ab6b01c35e
Christoph Bratschi <bratschi@appamics.com>
2016-09-22 17:50:39 +0200

    layer test results

--- demos/tests/layers.js
+++ demos/tests/layers.js
@@ -8,15 +8,32 @@ if (process.argv.length == 2) {
 /*
  * Results:
  *
- *  1) Raspberry Pi: 1080p
+ *  1) Raspberry Pi: 1080p (2016-09-22)
  *
- *    TODO
+ *    - 5:  60 fps (10.3 MP)
+ *    - 6:  52 fps (12.4 MP)
+ *    - 10: 32 fps (20.7 MP)
+ *    - 20: 13 fps (41.4 MP)
+ *    - 40:  6 fps (82.9 MP)
+ *
+ *    => limit at 5 fullscreen layers (ca. 10 MP)
+ *
+ *  2) Raspberry Pi: 720p (2016-09-22)
+ *
+ *    - 12: 60 fps (11.1 MP)
+ *    - 14: 50 fps (12.9 MP
+ *    - 20: 32 fps (18.4 MP)
+ *    - 40: 15 fps (36.9 MP)
+ *
+ *    => limit at 12 fullscreen layers (ca. 11 MP)
  */
 
 const layers = process.argv[2];
 const amino = require('../../main.js');
 
 const gfx = new amino.AminoGfx({
+    //resolution: '1080p@60',
+    //resolution: '720p@60',
     perspective: {
         orthographic: false
     }
@@ -32,6 +49,9 @@ gfx.start(function (err) {
     let root = this.createGroup();
 
     this.setRoot(root);
+
+    //Note: not seeing any difference
+    //root.depth(true);
 
     //layers
     for (let i = 0; i < layers; i++) {
