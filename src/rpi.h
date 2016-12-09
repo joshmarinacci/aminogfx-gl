19b11d3e9cc21471a6fa1434f2eaf6954b541c36
Christoph Bratschi <bratschi@appamics.com>
2016-12-09 18:51:37 +0100

    cleanup header file

--- src/rpi.h
+++ src/rpi.h
@@ -5,11 +5,90 @@
 #include "renderer.h"
 #include "rpi_video.h"
 
+#include "bcm_host.h"
+#include "interface/vchiq_arm/vchiq_if.h"
+
+#include <semaphore.h>
+#include <linux/input.h>
+
 class AminoGfxRPiFactory : public AminoJSObjectFactory {
 public:
     AminoGfxRPiFactory(Nan::FunctionCallback callback);
 
     AminoJSObject* create() override;
+};
+
+/**
+ * Raspberry Pi AminoGfx implementation.
+ */
+class AminoGfxRPi : public AminoGfx {
+public:
+    AminoGfxRPi();
+    ~AminoGfxRPi();
+
+    static AminoGfxRPiFactory* getFactory();
+    static NAN_MODULE_INIT(Init);
+
+    EGLImageKHR createEGLImage(GLuint textureId);
+
+private:
+    static bool glESInitialized;
+
+    //OpenGL ES
+    EGLDisplay display = EGL_NO_DISPLAY;
+    EGLContext context = EGL_NO_CONTEXT;
+    EGLSurface surface = EGL_NO_SURFACE;
+    EGLConfig config;
+    uint32_t screenW = 0;
+    uint32_t screenH = 0;
+
+    //resolution
+    static sem_t resSem;
+    static bool resSemValid;
+
+    //input
+    std::vector<int> fds;
+    int mouse_x = 0;
+    int mouse_y = 0;
+
+    static NAN_METHOD(New);
+
+    void setup() override;
+    void initEGL();
+
+    static void tvservice_cb(void *callback_data, uint32_t reason, uint32_t param1, uint32_t param2);
+    static void tvservice_cb(void *callback_data, uint32_t reason, uint32_t param1, uint32_t param2);
+
+    void destroy() override;
+    void destroyAminoGfxRPi();
+
+    bool getScreenInfo(int &w, int &h, int &refreshRate, bool &fullscreen) override;
+
+    void forceHdmiMode(uint32_t code);
+
+    void populateRuntimeProperties(v8::Local<v8::Object> &obj) override;
+    void initRenderer() override;
+
+    bool startsWith(const char *pre, const char *str);
+    void initInput();
+
+    void start() override;
+    bool bindContext() override;
+    void renderingDone() override;
+    void handleSystemEvents() override;
+
+    void processInputs();
+    void handleEvent(input_event ev);
+    void dump_event(struct input_event *event);
+
+    void updateWindowSize() override;
+    void updateWindowPosition() override;
+    void updateWindowTitle() override;
+
+    void atlasTextureHasChanged(texture_atlas_t *atlas) override;
+    void atlasTextureHasChangedHandler(JSCallbackUpdate *update);
+
+    AminoVideoPlayer *createVideoPlayer(AminoTexture *texture, AminoVideo *video) override;
 };
 
 #endif
