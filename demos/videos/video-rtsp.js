812b4c07494baee374709818cafa5cb5fe95a140
Christoph Bratschi <bratschi@appamics.com>
2017-03-10 18:57:57 +0100

    Digoo test case

--- demos/videos/video-rtsp.js
+++ demos/videos/video-rtsp.js
@@ -10,6 +10,9 @@ const player = require('./player');
 player.playVideo({
     //Note: TCP stream needed (UDP timeout, retrying with TCP)
     src: 'rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4'
+
+    //Digoo M1Q (default password, on local network => modify parameters; see https://www.ispyconnect.com/man.aspx?n=Digoo#)
+    //src: 'rtsp://admin:20160404@192.168.1.89/onvif1'
 }, (err, video) => {
     //empty
 });
