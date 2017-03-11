18241b8bd1b682b97a6319b04208a77c6b6dd280
Christoph Bratschi <bratschi@appamics.com>
2017-03-11 21:04:41 +0100

    split RTSP tests

---
+++ demos/videos/video-rtsp-digoo.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const path = require('path');
+const player = require('./player');
+
+/*
+ * Play RTSP stream.
+ */
+const ip = '192.168.1.89';
+
+player.playVideo({
+    //
+    //  Digoo M1Q (default password, on local network => modify parameters; see https://www.ispyconnect.com/man.aspx?n=Digoo#)
+    //
+
+    src: 'rtsp://admin:20160404@' + ip + '/onvif1'
+    //src: 'rtsp://admin:20160404@' + ip + '/onvif2' //low res
+
+    //not working
+    //src: 'rtsp://admin:20160404@' + ip + '/live/ch00_0' //HD
+    //src: 'rtsp://admin:20160404@' + ip + '/live/ch00_1' //SD
+}, (err, video) => {
+    //empty
+});
