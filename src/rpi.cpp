a476232268885a7288c9b78869ff848ef69acd9d
Christoph Bratschi <bratschi@appamics.com>
2017-03-13 18:38:10 +0100

    typecast fix

--- src/rpi.cpp
+++ src/rpi.cpp
@@ -929,7 +929,7 @@ EGLImageKHR AminoGfxRPi::createEGLImage(GLuint textureId) {
  */
 void AminoGfxRPi::destroyEGLImage(EGLImageKHR eglImage) {
     //switch to rendering thread
-    enqueueValueUpdate(0, eglImage, static_cast<asyncValueCallback>(&AminoGfxRPi::destroyEGLImageHandler));
+    enqueueValueUpdate(0, (void *)eglImage, static_cast<asyncValueCallback>(&AminoGfxRPi::destroyEGLImageHandler));
 }
 
 /**
