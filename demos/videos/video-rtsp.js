a41569e16b924a677d8ba53220fe4568bd0dc95f
Christoph Bratschi <bratschi@appamics.com>
2017-03-16 12:31:28 +0100

    different RTSP demo

--- demos/videos/video-rtsp.js
+++ demos/videos/video-rtsp.js
@@ -8,7 +8,13 @@ const player = require('./player');
  */
 
 player.playVideo({
-    src: 'rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4',
+    //low res
+    //src: 'rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4',
+    //src: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov',
+
+    //720p
+    //src: 'rtsp://mm2.pcslab.com/mm/7m1000.mp4',
+    src: 'rtsp://mm2.pcslab.com/mm/7m2000.mp4',
 
     //Note: TCP stream needed (UDP timeout, retrying with TCP)
     opts: 'rtsp_transport=tcp'
