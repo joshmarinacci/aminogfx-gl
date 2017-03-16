18ea1f52ec2179f2d9751c6f62c0ca1c01864ba4
Christoph Bratschi <bratschi@appamics.com>
2017-03-16 12:34:34 +0100

    another stream

--- demos/videos/video-rtsp.js
+++ demos/videos/video-rtsp.js
@@ -12,9 +12,12 @@ player.playVideo({
     //src: 'rtsp://mpv.cdn3.bigCDN.com:554/bigCDN/definst/mp4:bigbuckbunnyiphone_400.mp4',
     //src: 'rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov',
 
-    //720p
+    //720p (h264)
+    src: 'rtsp://mm2.pcslab.com/mm/7h1500.mp4',
+
+    //720p (mpeg4)  -> not support on RPi yet
     //src: 'rtsp://mm2.pcslab.com/mm/7m1000.mp4',
-    src: 'rtsp://mm2.pcslab.com/mm/7m2000.mp4',
+    //src: 'rtsp://mm2.pcslab.com/mm/7m2000.mp4',
 
     //Note: TCP stream needed (UDP timeout, retrying with TCP)
     opts: 'rtsp_transport=tcp'
