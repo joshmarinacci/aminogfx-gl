a3ed3f6907a3e01ef4292bcdbf691394b7c98cb2
Christoph Bratschi <bratschi@appamics.com>
2017-03-24 16:43:38 +0100

    modified stop test case

--- demos/videos/stop.js
+++ demos/videos/stop.js
@@ -1,15 +1,22 @@
 'use strict';
 
 const player = require('./player');
+const path = require('path');
 
 /*
  * Play RTSP stream & stop after 4 seconds.
  */
 
 player.playVideo({
+    //local file
+    src: path.join(__dirname, 'trailer_iphone.m4v'),
+
     //720p (h264)
+    /*
     src: 'rtsp://mm2.pcslab.com/mm/7h1500.mp4',
     opts: 'rtsp_transport=tcp',
+    */
+
     ready: video => {
         setTimeout(() => {
             player.measureStart();
